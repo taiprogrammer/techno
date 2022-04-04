03e531903c72c573c5c8e228ac5c8fa08f74361b
taiprogrammer <taiza.marques@sptech.school>
2022-04-04 09:50:34 -0300

    card component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Card text="Taiza Marques" />
+    <Card />
   </div>
 </template>
 
@@ -18,10 +18,14 @@ export default {
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap");
 
-body {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+body,
+h1,
+h2,
+ul,
+li,
+p {
+  padding: 0px;
+  margin: 0px;
 }
 
 #app {
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,30 +1,39 @@
 <template>
   <div>
-    <CardWrapper>{{ text }}</CardWrapper>
-    <Menu />
+    <Menu>
+      <Logo></Logo>
+    </Menu>
   </div>
 </template>
 
 <script>
 import styled from "vue-styled-components";
 
-export const CardWrapper = styled.p`
-  color: red;
+export const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 900px;
+  padding: 25px 0;
+  margin: 0 auto;
+  background-color: #011;
 `;
 
-export const Menu = styled.div`
-  height: 10px;
-  background-color: #000;
+export const Logo = styled.h1`
+  color: #fff;
+  padding-left: 10px;
+`;
+
+export const Banner = styled.section`
+  width: 100vw;
+  height: 30vh;
+  background-color: cornflowerblue;
 `;
 
 export default {
-  props: {
-    text: String,
-  },
   name: "Card",
   components: {
-    CardWrapper,
     Menu,
+    Logo,
   },
 };
 </script>
